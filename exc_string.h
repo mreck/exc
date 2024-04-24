76c85ab1b6c628c535b4fd2171a3cb6c4f62abc8
Mathias Reck <mathias.reck@gmail.com>
2024-04-24 23:10:47 +0200

    fix names

--- exc_string.h
+++ exc_string.h
@@ -6,7 +6,7 @@
 typedef struct {
     char *begin;
     int   len;
-} StringView;
+} EXC_String_View;
 
 #define EXC_SV_PRINTF_FMT "%.*s"
 #define EXC_SV_PRINTF_ARGS(sv) (sv).len, (sv).begin
@@ -14,15 +14,15 @@ typedef struct {
 #define exc_sv_end(sv) ((sv).begin + (sv).len)
 #define exc_sv_last(sv) (exc_sv_end(sv) - 1)
 
-StringView exc_sv_from_cstr(char *str, int len);
+EXC_String_View exc_sv_from_cstr(char *str, int len);
 
-StringView exc_sv_trim_space(StringView sv);
-StringView exc_sv_trim_space_left(StringView sv);
-StringView exc_sv_trim_space_right(StringView sv);
+EXC_String_View exc_sv_trim_space(EXC_String_View sv);
+EXC_String_View exc_sv_trim_space_left(EXC_String_View sv);
+EXC_String_View exc_sv_trim_space_right(EXC_String_View sv);
 
-int exc_sv_index_of(StringView sv, char c);
+int exc_sv_index_of(EXC_String_View sv, char c);
 
-void exc_sv_copy(void *dst, int dst_cap, StringView sv);
+void exc_sv_copy(void *dst, int dst_cap, EXC_String_View sv);
 
 #endif // EXC_STRING
 
@@ -31,22 +31,22 @@ void exc_sv_copy(void *dst, int dst_cap, StringView sv);
 #include <ctype.h>
 #include <string.h>
 
-StringView exc_sv_from_cstr(char *begin, int len)
+EXC_String_View exc_sv_from_cstr(char *begin, int len)
 {
     if (len == 0) {
         len = strlen(begin);
     }
-    return (StringView) { begin, len };
+    return (EXC_String_View) { begin, len };
 }
 
-StringView exc_sv_trim_space(StringView sv)
+EXC_String_View exc_sv_trim_space(EXC_String_View sv)
 {
     return exc_sv_trim_space_right(exc_sv_trim_space_left(sv));
 }
 
-StringView exc_sv_trim_space_left(StringView sv)
+EXC_String_View exc_sv_trim_space_left(EXC_String_View sv)
 {
-    StringView rval = { sv.begin, sv.len };
+    EXC_String_View rval = { sv.begin, sv.len };
     while (rval.len > 0 && isspace(*rval.begin)) {
         ++rval.begin;
         --rval.len;
@@ -54,16 +54,16 @@ StringView exc_sv_trim_space_left(StringView sv)
     return rval;
 }
 
-StringView exc_sv_trim_space_right(StringView sv)
+EXC_String_View exc_sv_trim_space_right(EXC_String_View sv)
 {
-    StringView rval = { sv.begin, sv.len };
+    EXC_String_View rval = { sv.begin, sv.len };
     while (rval.len > 0 && isspace(*exc_sv_last(rval))) {
         --rval.len;
     }
     return rval;
 }
 
-int exc_sv_index_of(StringView sv, char c)
+int exc_sv_index_of(EXC_String_View sv, char c)
 {
     for (int i = 0; i < sv.len; i++) {
         if (sv.begin[i] == c) {
@@ -73,7 +73,7 @@ int exc_sv_index_of(StringView sv, char c)
     return -1;
 }
 
-void exc_sv_copy(void *dst, int dst_cap, StringView sv)
+void exc_sv_copy(void *dst, int dst_cap, EXC_String_View sv)
 {
     int n = sv.len > dst_cap ? dst_cap : sv.len;
     memcpy(dst, sv.begin, n);
