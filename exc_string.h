442d4ee32932aa3a14788fa113bcc1f4520a1017
Mathias Reck <mathias.reck@gmail.com>
2024-04-29 23:31:35 +0200

    add exc_sv_parse_number()

--- exc_string.h
+++ exc_string.h
@@ -14,6 +14,7 @@ typedef struct {
 
 #define exc_sv_end(sv) ((sv).begin + (sv).len)
 #define exc_sv_last(sv) (exc_sv_end(sv) - 1)
+#define exc_sv_size(sv) ((((sv).begin) + (sv).len) - (sv).begin)
 
 EXC_String_View exc_sv_from_cstr(char *str, int len);
 
@@ -22,8 +23,9 @@ EXC_String_View exc_sv_trim_space_left(EXC_String_View sv);
 EXC_String_View exc_sv_trim_space_right(EXC_String_View sv);
 
 int exc_sv_index_of(EXC_String_View sv, char c);
+int exc_sv_parse_number(EXC_String_View sv, long long *n, int base);
 
-void exc_sv_copy(void *dst, int dst_cap, EXC_String_View sv);
+void exc_sv_copy(void *dst, int dst_cap, EXC_String_View sv, bool nullterm);
 
 typedef struct {
     char *str;
@@ -90,10 +92,28 @@ int exc_sv_index_of(EXC_String_View sv, char c)
     return -1;
 }
 
-void exc_sv_copy(void *dst, int dst_cap, EXC_String_View sv)
+int exc_sv_parse_number(EXC_String_View sv, long long *n, int base)
 {
-    int n = sv.len > dst_cap ? dst_cap : sv.len;
+    sv = exc_sv_trim_space(sv);
+    char buff[32];
+    char *end;
+    if ((unsigned long)exc_sv_size(sv) > (sizeof(buff) - 1)) return 1;
+    exc_sv_copy(buff, sizeof(buff), sv, true);
+    errno = 0;
+    long long res = strtoll(buff, &end, base);
+    if (errno != 0) return 1;
+    *n = res;
+    return 0;
+}
+
+void exc_sv_copy(void *dst, int dst_cap, EXC_String_View sv, bool nullterm)
+{
+    int cap = nullterm ? dst_cap - 1 : dst_cap;
+    int n = sv.len > cap ? cap : sv.len;
     memcpy(dst, sv.begin, n);
+    if (nullterm) {
+        ((char*)(dst))[n] = '\0';
+    }
 }
 
 void exc_string_builder_free(EXC_String_Builder *b)
